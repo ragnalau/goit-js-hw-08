53bf91c7db31a44ae93694094375c16c0a854b00
Reut Mihai <reutmihai@icloud.com>
2024-08-22 15:19:09 +0300

    exercitiul 2 rezolvat

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+
+player.on('loaded', () => {
+  const savedTime = localStorage.getItem(STORAGE_KEY);
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
+
+
+const saveCurrentTime = throttle(function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}, 1000); 
+
+player.on('timeupdate', saveCurrentTime);
