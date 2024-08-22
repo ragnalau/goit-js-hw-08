510d86b8499df739df3da737a126c5c7124c6811
Reut Mihai <reutmihai@icloud.com>
2024-08-22 14:07:45 +0300

    exercitiu 1 rezolvat

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+const createGalleryMarkup = items => {
+  return items
+    .map(item => {
+      const { preview, original, description } = item;
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+         />
+          </a>
+</li>`;
+    })
+    .join('');
+};
+
+const renderGallery = () => {
+    const galleryContainer = document.querySelector('.gallery');
+  galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+};
+
+renderGallery();
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
