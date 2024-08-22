262dffdb334a40d5364b08c95a39ebf10838cd4e
Reut Mihai <reutmihai@icloud.com>
2024-08-22 18:51:29 +0300

    exercitiu 3 rezolvat

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+/*
+1.Urmăriți în formular evenimentul de input și la fiecare modificare să se salveze local un obiect cu câmpurile email și message, unde vor fi 
+stocate valorile curente ale câmpurilor din formular. Cheia obiectului va fi "feedback-form-state"
+2.La încărcarea paginii, verificați starea storage-ului, iar dacă există date salvate, completați câmpurile formularului cu aceste date. În 
+caz contrar, câmpurile vor fi goale.
+3.Când se trimite formularul, la evenimentul submit, ștergeți câmpurile din local storage și afișați în consolă obiectul cu câmpurile email, 
+message și valorile lor curente.
+4.Asigurați-vă că datele vor fi stocate și actualizate doar o dată la 500 de milisecunde. Pentru a face acest lucru, adăugați la proiect și 
+utilizați librăria lodash.throttle.
+*/
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const saveToLocalStorage = throttle(() => {
+    const dataForm = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+
+    save(STORAGE_KEY, dataForm);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}, 500);
+
+const loadFromLocalStorage = () => {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if(savedData) {
+        const { email, message } = JSON.parse(savedData);
+        form.elements.email.value = email || '';
+        form.elements.message.value = message || '';
+    }
+}
+
+
+form.addEventListener('input', saveToLocalStorage);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  console.log('Form submitted with data:', formData);
+
+
+//   localStorage.removeItem(STORAGE_KEY);
+
+
+  form.reset();
+});
+
+loadFromLocalStorage();
